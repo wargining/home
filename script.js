f42bdadf5920cd0e1a767ad07ab052869929906f
wargining <140636848+wargining@users.noreply.github.com>
2026-07-23 16:20:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+if (toggle && nav) {
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+}
